0c5da257674c7830c0908d3aa71f2ec91839571b
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-07 16:45:27 +0330

    Default profile spring close #9

--- src/main/java/com/example/spring6di/services/EnglishGreetingService.java
+++ src/main/java/com/example/spring6di/services/EnglishGreetingService.java
@@ -2,7 +2,7 @@ package com.example.spring6di.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18NService")
 public class EnglishGreetingService implements GreetingService {
     @Override

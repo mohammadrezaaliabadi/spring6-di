840eeeb2c1c1f6eeb58d09bae60ab829821e101c
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-07 16:44:20 +0330

    Default profile spring close #8

---
+++ src/main/java/com/example/spring6di/services/EnglishGreetingService.java
@@ -0,0 +1,12 @@
+package com.example.spring6di.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
---
+++ src/test/java/com/example/spring6di/controllers/Myi18NControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.spring6di.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("ES")
+@SpringBootTest
+class Myi18NControllerTest {
+
+    @Autowired
+    Myi18NController myi18NController;
+    @Test
+    void sayHello() {
+
+        System.out.println(myi18NController.sayHello());
+    }
+}
